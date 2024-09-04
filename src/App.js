6abf43a9b9c370f5732d82d884f77e2b000b6ce3
ericlinjh <ericlinjh@hotmail.com>
2024-09-04 17:58:23 +0800

    Made hero section a bit more responsive

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.scss';
 import gradient from './background/gradient.svg'
 import noise from './background/noise.svg'
 import signature from './hero-section/SIGNATURE.json'
-import carousel from './hero-section/carousel.json'
+//import carousel from './hero-section/carousel.json'
 import resume from './resume.pdf'
 import { motion, useMotionValueEvent, useScroll } from 'framer-motion'
 
@@ -81,7 +81,7 @@ function App() {
               >
                 I'm
               </motion.h1>
-              <div class="text-container">
+              <div className="text-container">
                 <motion.h1
                   variants={introTextAnimation}
                   initial="initial"
@@ -95,19 +95,19 @@ function App() {
           </motion.div>
         </div>
 
-        <motion.div 
-          className="right-hero"
-          variants={introFade}
-          initial="hidden"
-          animate="visible"
-        >
-            <Lottie
-              animationData={carousel}
-              loop={true}
-              autoplay={true}
-            />
-        </motion.div>
-       
+        {/*<motion.div */}
+        {/*  className="right-hero"*/}
+        {/*  variants={introFade}*/}
+        {/*  initial="hidden"*/}
+        {/*  animate="visible"*/}
+        {/*>*/}
+        {/*    <Lottie*/}
+        {/*      animationData={carousel}*/}
+        {/*      loop={true}*/}
+        {/*      autoplay={true}*/}
+        {/*    />*/}
+        {/*</motion.div>*/}
+
 
         <Lottie
         animationData={signature}
@@ -130,7 +130,7 @@ function App() {
         src={noise}
         variants={introFade}
         initial="hidden"
-        animate={{ opacity: "15%" }}
+        animate={{ opacity: "40%" }}
       />
       <motion.img
         id="background-gradient"
